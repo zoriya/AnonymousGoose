d811123166ac1fa9d583960d9b682bbd64d0ef03
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-04-24 22:45:27 +0200

    Allowing the user to exit the app by pressing escape

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 #!/usr/bin/env python3
-import os
 import time
+import pyxhook
 
 from term_utils import TermUtils
 from command_helper import CommandHelper
 
-TermUtils.print("YOU HAVE BEEN HACKED.\n")
 
-while True:
-	if CommandHelper.run("killall htop") == 0 or CommandHelper.run("killall top") == 0:
-		TermUtils.print("You tough that this will be as easy as this?\n")
-	time.sleep(1)
+class AnonymousGoose:
+	def __init__(self):
+		self.should_exit = False
+		self.stopped = False
+		self.tricks = []
+		self.keyboard_listener = pyxhook.HookManager()
+		self.keyboard_listener.KeyUp = self.key_pressed
+		self.keyboard_listener.HookKeyboard()
+		self.keyboard_listener.start()
+		TermUtils.print("YOU HAVE BEEN HACKED.\n")
+		self.run()
+
+	def __del__(self):
+		if not self.stopped:
+			self.keyboard_listener.cancel()
+
+	def run(self):
+		while not self.should_exit:
+			if CommandHelper.run("killall htop") == 0 or CommandHelper.run("killall top") == 0:
+				TermUtils.print("You tough that this will be as easy as this?\n")
+			time.sleep(1)
+
+	def key_pressed(self, key):
+		if key.Ascii == 27:
+			self.should_exit = True
+			
+	def stop(self):
+		if self.stopped:
+			return
+		self.keyboard_listener.cancel()
+		self.stopped = True
+
+
+if __name__ == "__main__":
+	goose = AnonymousGoose()
+	goose.stop()
