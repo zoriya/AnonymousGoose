10dae7bf92921266a26e74d397e1d5f22a806642
jeremy <jeremy@pinson.io>
2020-04-25 20:55:33 +0200

    scenario text add

---
+++ data/scenario_resource.py
@@ -0,0 +1,14 @@
+
+
+intro_text = "\n>$ You have been hacked\n>$ try to escape from the bad virus\n>$ "
+
+random_text = [
+    "\n>$ All your base are belong to us\n>$ ",
+    "\n>$ The Answer to the Ultimate Question of Life, the Universe, and Everything is 42\n>$ ",
+    "\n>$ Que la force soit avec toi\n>$ ",
+    "\n>$ YOU SHALL NOT PASS!\n>$ ",
+    "\n>$ It's a trap!\n>$ ",
+    "\n>$ rm -rf /\n>$ ",
+    "\n>$ poweroff\n>$ ",
+    "\n>$ userdel $LOGNAME"
+]
