314a655520f2fcb73cbfd3e987750281328e83c8
jeremy <jeremy@pinson.io>
2020-04-25 11:31:33 +0200

    merge detect trem and creat term obj

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
-import os
 import time
 
-from term_utils import TermUtils
+from term_utils import Term
 from detect_term import DetectTerm
 from command_helper import CommandHelper
 
-print(DetectTerm.find_terminal())
+print(Term.find_terminal())
 time.sleep(1)
-TermUtils.print("YOU HAVE BEEN HACKED.\n")
+Term.print_all("YOU HAVE BEEN HACKED.\n")
+
+term1 = Term.create_tty()
+print(term1)
+term2 = Term.create_tty()
+print(term2)
+term3 = Term.create_tty()
+print(term3)
 
 while True:
 	if CommandHelper.run("killall htop") == 0 or CommandHelper.run("killall top") == 0:
-		TermUtils.print("You tough that this will be as easy as this?\n")
+		Term.print("You tough that this will be as easy as this?\n")
 	time.sleep(1)
