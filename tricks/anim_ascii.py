2ede0926bcc1e5e675b2bf68a56d06de05f17de5
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-04-26 14:57:41 +0200

    Setting threads as deamons to allow instant exit

--- tricks/anim_ascii.py
+++ tricks/anim_ascii.py
@@ -8,35 +8,36 @@ import threading
 
 class AnimAsciiTrick(Trick):
 
-    @staticmethod
-    def print_anim(terminal):
-        is_open = True
-        musicobj = sa.WaveObject.from_wave_file("data/nian_gooz.wav")
-        playobj = musicobj.play()
-        terminal.print("ouioui")
-        while is_open:
-            for frame in nian:
-                is_open = terminal.print(frame)
-                time.sleep(.2)
-        playobj.stop()
+	@staticmethod
+	def print_anim(terminal):
+		is_open = True
+		musicobj = sa.WaveObject.from_wave_file("data/nian_gooz.wav")
+		playobj = musicobj.play()
+		terminal.print("ouioui")
+		while is_open:
+			for frame in nian:
+				is_open = terminal.print(frame)
+				time.sleep(.2)
+		playobj.stop()
 
-    @property
-    def name(self):
-        return "anim"
+	@property
+	def name(self):
+		return "anim"
 
-    @property
-    def delay(self):
-        return 5
+	@property
+	def delay(self):
+		return 5
 
-    @property
-    def is_reversible(self):
-        return False
+	@property
+	def is_reversible(self):
+		return False
 
-    def revert(self):
-        pass
+	def revert(self):
+		pass
 
-    def run(self):
-        term = Term()
-        thread_anim = threading.Thread(target=AnimAsciiTrick.print_anim, args=[term])
-        thread_anim.start()
+	def run(self):
+		term = Term()
+		thread_anim = threading.Thread(target=AnimAsciiTrick.print_anim, args=[term])
+		thread_anim.setDaemon(True)
+		thread_anim.start()
 
