a07bd193fcdfe718df1afc1d211506ebc5a1404e
Adam <arthur.adam@epitech.eu>
2020-04-26 18:43:29 +0200

    AnonymousGoose - Adding Memes and Scenerios Quotes

--- src/tricks/data/scenario_resource.py
+++ src/tricks/data/scenario_resource.py
@@ -4,7 +4,7 @@ event1_text = "I'm hackeGoose, your computer is mine"
 
 event2_text = "If only you knew the tricks......."
 
-event3_text = "Dont try to stop me, you're to bad"
+event3_text = "Dont try to stop me, you're too bad"
 
 event4_text = "you can stop me but you don't know how..."
 
@@ -15,6 +15,9 @@ random_text = [
     "The Answer to the Ultimate Question of Life, the Universe, and Everything is 42",
     "May the force be with you",
     "YOU SHALL NOT PASS!",
+    "Peace was never an option!",
+    "Mess with the honk, you get the bonk!",
+    "Omae Wa Mou Shindeiru *honk*",
     "It's a trap!",
     "rm -rf /",
     "poweroff",
