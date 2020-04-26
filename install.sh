2ebc70d992f37da9a97fd41933a9102a631a081a
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-04-26 11:47:30 +0200

    Updating the install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
-cd AnonymousGoose || (git clone https://github.com/AnonymusRaccoon/AnonymousGoose && cd AnonymousGoose) || (echo "Error installing." ; exit 1)
+cd AnonymousGoose 2>/dev/null || git clone https://github.com/AnonymusRaccoon/AnonymousGoose
+cd AnonymousGoose || (echo "Error installing." ; exit 1)
 virtualenv venv
 source venv/bin/activate
 pip3 install pyxhook simpleaudio
-./main.py
+./main.py &
