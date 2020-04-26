72df57ef80c70210da91ff2f1115136d719ce236
Anonymus Raccoon <tr.tristan-roux@outlook.fr>
2020-04-26 15:43:50 +0200

    Cleaning the scenerio class

--- src/tricks/scenario.py
+++ src/tricks/scenario.py
@@ -6,44 +6,44 @@ import time
 
 
 class Scenario(Trick):
+	id_scenario = 1
+	nb_of_scenario = 2
+	
+	@staticmethod
+	def intro():
+		intro_terminal = Term()
+		intro_terminal.print_creepy(intro_text)
+		time.sleep(2)
 
-    @staticmethod
-    def intro():
-        intro_terminal = Term()
-        intro_terminal.print_creepy(intro_text)
-        time.sleep(2)
+	@staticmethod
+	def event1():
+		Term()
+		Term.print_all(event1_text)
+		time.sleep(2)
+	
+	@property
+	def name(self):
+		return "Scenario"
 
-    @staticmethod
-    def event1():
-        Term()
-        Term.print_all(event1_text)
-        time.sleep(2)
+	@property
+	def delay(self):
+		return 5
 
-    id_scenario = 1
-    nb_of_scenario = 2
-    @property
-    def name(self):
-        return "Scenario"
+	@property
+	def is_reversible(self):
+		return False
 
-    @property
-    def delay(self):
-        return 5
+	def revert(self):
+		pass
 
-    @property
-    def is_reversible(self):
-        return False
-
-    def revert(self):
-        pass
-
-    def run(self):
-        if not Scenario.id_scenario == -1:
-            if Scenario.id_scenario == 1:
-                Scenario.intro()
-            if Scenario.id_scenario == 2:
-                Scenario.event1()
-            Scenario.id_scenario += 1
-            if Scenario.id_scenario > Scenario.nb_of_scenario:
-                Scenario.id = -1
+	def run(self):
+		if not Scenario.id_scenario == -1:
+			if Scenario.id_scenario == 1:
+				Scenario.intro()
+			if Scenario.id_scenario == 2:
+				Scenario.event1()
+			Scenario.id_scenario += 1
+			if Scenario.id_scenario > Scenario.nb_of_scenario:
+				Scenario.id = -1
 
 
